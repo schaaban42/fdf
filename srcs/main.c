6bc46e376cb2e88aa6157bcc319496b4b3102fcc
myubuntu <schaaban@student.42.fr>
2018-01-22 17:25:49 +0100

    RM -rf objs + modif main.c parser.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: schaaban <schaaban@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/11 12:28:50 by schaaban          #+#    #+#             */
-/*   Updated: 2018/01/19 12:22:54 by schaaban         ###   ########.fr       */
+/*   Updated: 2018/01/22 17:15:44 by schaaban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,16 @@ int		key_pressed(int keycode, void *param)
 	t_mlx	*mlx;
 
 	mlx = (t_mlx*)param;
-	if (keycode == 53)
+	if (keycode == 65307)
 		exit(0);
+	mlx_pixel_put(mlx->core, mlx->window, 100, 100, 0x0000FF);
 	return (0);
 }
 
 int		error_handler(int error)
 {
+	if (error == -2)
+		ft_printf("error: could not initialize\n");
 	if (error == -1)
 		ft_printf("error: m-allocation failed\n");
 	if (error == 0)
@@ -57,18 +60,19 @@ t_mlx	*mlx_win_init(void)
 int		main(int argc, char **argv)
 {
 	t_mlx	*mlx;
+	int		**map;
+	int		map_width;
 	int		fd;
 
 	if (argc != 2)
 		return (error_handler(0));
 	if (!(fd = open(argv[1], O_RDONLY)))
 		return (error_handler(1));
-	if (!(mlx = mlx_win_init()))
-		return (0);
-	if (!parse_file(fd))
-		return (0);
+	if (!(mlx = mlx_win_init()) || !(map = parse_file(fd, &map_width)))
+		return (error_handler(-2));
 	if (close(fd) != 0)
 		return (error_handler(3));
+	mlx_pixel_put(mlx->core, mlx->window, 250, 250, 0xFF0000);
 	mlx_key_hook(mlx->window, key_pressed, (void*)mlx);
 	mlx_loop(mlx->core);
 	return (0);
--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: schaaban <schaaban@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/16 17:35:33 by schaaban          #+#    #+#             */
-/*   Updated: 2018/01/19 15:37:36 by schaaban         ###   ########.fr       */
+/*   Updated: 2018/01/22 17:19:10 by schaaban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,65 +14,16 @@
 #include "fdf.h"
 #include <stdlib.h>
 
-static char	**prs_lines_ok(char *str, int *tab_width)
+static int	**prs_getmap(char *str, int *map_width)
 {
-	char	**lines;
-	char	**line;
-	int		i;
-	int		j;
-
-	i = -1;
-	lines = ft_strsplit(str, '\n');
-	while (lines[++i] != NULL)
-	{
-		j = -1;
-		line = ft_strsplit(lines[i], ' ');
-		while (line[++j] != NULL)
-		{
-	ft_printf("BONJOUR CA VA\n");
-			if (j > 0 && (int)ft_strlen(line[j]) != *tab_width)
-			{
-				ft_a2ddel((void**)line);
-				ft_a2ddel((void**)lines);
-				return (NULL);
-			}
-			*tab_width = ft_strlen(line[j]);
-		}
-	}
-	ft_a2ddel((void**)line);
-	return (lines);
+	str = "binberoigj";
+	*map_width = 9;
+	map_width = (int*)str;
+	str = (char*)map_width;
+	return ((int**)67);
 }
 
-static int	**prs_getmap(char *str, int *tab_width)
-{
-	int		**map;
-	char	**lines;
-	char	**line_split;
-	int		i;
-	int		j;
-
-	if (!(lines = prs_lines_ok(str, tab_width)))
-		return (NULL);
-	i = 0;
-	if (!(map = (int**)malloc(sizeof(int*) * 
-		(ft_a2dlen((const void**)lines) + 1))))
-		return (NULL);
-	while (lines[i] != NULL)
-	{
-		j = 0;
-		line_split = ft_strsplit(lines[i], ' ');
-		if (!(map[i] = (int*)malloc(sizeof(int) * (*tab_width))))
-			return (NULL);
-		while (j < (*tab_width))
-		{
-			map[i][j] = ft_atoi(line_split[j]);
-			j++;
-		}
-	}
-	return (map);
-}
-
-int			**parse_file(int fd, int *tab_width)
+int			**parse_file(int fd, int *map_width)
 {
 	int		**map;
 	char	*str;
@@ -87,7 +38,8 @@ int			**parse_file(int fd, int *tab_width)
 	}
 	if (!str)
 		return (NULL);
-	map = prs_getmap(str, tab_width);
+	ft_printf("Str : %s", str);
+	map = prs_getmap(str, map_width);
 	ft_memdel((void**)&str);
 	return (map);
 }
